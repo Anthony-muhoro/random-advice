d26da094a6cb22bdf5b579352ef5309e2490baf1
Anthony-muhoro <anthonymuhoro2003@gmail.com>
2025-06-23 16:43:58 +0300

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import React, { useState } from "react";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+const Home = () => {
+  const [advice, setAdvice] = useState("Do nothing be nothing");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-export default function Home() {
-  return <Welcome />;
-}
+  const getRandomAdvice = async () => {
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full items-center  min-h-screen bg-orange-900 text-white gap-4">
+      <h1 className="mt-12">Get a Random Advice</h1>
+      <h1 className="text-2xl font-bold text-center">
+        {error ? "something went wrong. Please Try again" : advice}
+      </h1>
+      <button
+        onClick={getRandomAdvice}
+        disabled={loading}
+        className="px-4 py-2 bg-blue-600 rounded hover:bg-gray-800 cursor-pointer"
+      >
+        {loading ? "Loading..." : "Get Advice"}
+      </button>
+    </div>
+  );
+};
+
+export default Home;
